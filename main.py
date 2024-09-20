2ecc6ceee4d6143a50f3367ed0ff6ab21e9dc50b
fenroe <scrambles9494@gmail.com>
2024-09-20 11:23:03 +0300

    Feat: created game loop, drew onto screen

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+from pygame import init, display, event, QUIT
 from constants import *
 
 def main():
+    init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for e in event.get():
+            if e.type == QUIT:
+                return
+        screen.fill("black")
+        display.flip()
 
 if __name__ == "__main__":
     main()
