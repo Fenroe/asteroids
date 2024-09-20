d50a24ef39c1a34385d2140a7ea94c5973e9f661
fenroe <scrambles9494@gmail.com>
2024-09-20 11:54:23 +0300

    Feat: limited refresh rate, created new classes, rendered player object on screen

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-from pygame import init, display, event, QUIT
+import pygame
 from constants import *
+from player import Player
 
 def main():
-    init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        for e in event.get():
-            if e.type == QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 return
         screen.fill("black")
-        display.flip()
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
